061cc73ff7fae5147fd9032109aff34ae895bcc5
Christian <chrisdaq@gmail.com>
2025-01-22 11:17:22 -0400

    import utilities into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "react-toastify/dist/ReactToastify.css";
+import "react-quill/dist/quill.snow.css";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ToastContainer position="top-right" />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
