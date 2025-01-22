2300c6573d75c8d74a27bd67f95e7f6ca46c995d
Christian <chrisdaq@gmail.com>
2025-01-22 12:00:12 -0400

    add Header, start defining routes on App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/layouts/Header";
+import { Home } from "./components/Home";
+
 function App() {
-  return <div>Hello, World!</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
