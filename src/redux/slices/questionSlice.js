7d9aab84d1d550add24c26abec666a4cefdae97a
Christian <chrisdaq@gmail.com>
2025-01-27 21:47:03 -0400

    add more actions

--- src/redux/slices/questionSlice.js
+++ src/redux/slices/questionSlice.js
@@ -6,8 +6,14 @@ const { VITE_BASE_URL } = getEnvironments();
 
 export const fetchQuestions = createAsyncThunk(
   "questions/fetchQuestions",
-  async ({ page }, { rejectWithValue }) => {
+  async ({ page, choosenTag, choosenUser }, { rejectWithValue }) => {
     let query = `${VITE_BASE_URL}/api/questions?page=${page}`;
+
+    if (choosenTag) {
+      query = `${VITE_BASE_URL}/api/tag/${choosenTag}/questions?page=${page}`;
+    } else if (choosenUser) {
+      query = `${VITE_BASE_URL}/api/user/questions?page=${page}`;
+    }
 
     try {
       const response = await axios.get(query);
@@ -25,10 +31,27 @@ const questionsSlice = createSlice({
     loading: false,
     page: 1,
     error: null,
+    choosenTag: "",
+    choosenUser: "",
   },
   reducers: {
     setPage: (state, action) => {
       state.page = action.payload;
+    },
+    filterQuestionsByTag: (state, action) => {
+      state.choosenTag = action.payload;
+      state.choosenUser = "";
+      state.page = 1;
+    },
+    filterQuestionsByUser: (state, action) => {
+      state.choosenUser = action.payload;
+      state.choosenTag = "";
+      state.page = 1;
+    },
+    clearFilter: (state) => {
+      state.choosenUser = "";
+      state.choosenTag = "";
+      state.page = 1;
     },
   },
   extraReducers: (builder) => {
@@ -48,6 +71,11 @@ const questionsSlice = createSlice({
   },
 });
 
-export const { setPage } = questionsSlice.actions;
+export const {
+  setPage,
+  filterQuestionsByTag,
+  filterQuestionsByUser,
+  clearFilter,
+} = questionsSlice.actions;
 
 export default questionsSlice.reducer;
