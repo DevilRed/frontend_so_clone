61f2fdd3d1526934cb89ae7fc2cc3c6150ad8dae
Christian <chrisdaq@gmail.com>
2025-02-02 17:47:11 -0400

    test setCurrentUser, setToken

--- src/redux/slices/userSlice.test.js
+++ src/redux/slices/userSlice.test.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import {
   userSlice,
   setCurrentUser,
@@ -17,6 +18,30 @@ describe("userSlice", () => {
     expect(state).toEqual({
       ...initialState,
       isLoggedIn: true,
+    });
+  });
+
+  it("should set current user", () => {
+    const user = {
+      id: 1,
+      name: "Thulio",
+      email: "thulio@gmail.com",
+      image: "user.png",
+      questionsCount: 5,
+      answersCount: 4,
+    };
+    const state = userSlice.reducer(initialState, setCurrentUser(user));
+    expect(state).toEqual({
+      ...initialState,
+      user,
+    });
+  });
+  it("should set token", () => {
+    const token = "abc123";
+    const state = userSlice.reducer(initialState, setToken(token));
+    expect(state).toEqual({
+      ...initialState,
+      token,
     });
   });
 });
