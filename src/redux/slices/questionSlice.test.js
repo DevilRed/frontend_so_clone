4b36490c326e08c4abc85725e2b8e50a15a6376e
Christian <chrisdaq@gmail.com>
2025-02-02 21:45:30 -0400

    add question slice fixture

--- src/redux/slices/questionSlice.test.js
+++ src/redux/slices/questionSlice.test.js
@@ -8,21 +8,7 @@ import {
   fetchNextPrevPage,
 } from "./questionSlice";
 
-const initialState = {
-  questions: {
-    data: [], // List of questions
-    meta: null, // Pagination metadata
-  },
-  question: null,
-  loading: false,
-  page: 1,
-  error: null,
-  choosenTag: "",
-  choosenUser: "",
-  nexPageLink: null,
-  prevPageLink: null,
-  showAll: false,
-};
+import { initialState } from "../../../tests/fixtures/questionSliceFixture";
 
 describe("questionSlice", () => {
   it("should set page", () => {
