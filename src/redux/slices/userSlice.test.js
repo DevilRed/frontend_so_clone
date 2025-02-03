bd7f742afc01e458a35c61b6560ec41820e62763
Christian <chrisdaq@gmail.com>
2025-02-02 21:38:29 -0400

    include fixtures, refactor test to include them

--- src/redux/slices/userSlice.test.js
+++ src/redux/slices/userSlice.test.js
@@ -5,12 +5,10 @@ import {
   setLoggedInOut,
   setToken,
 } from "./userSlice";
-
-const initialState = {
-  isLoggedIn: false,
-  token: "",
-  user: null,
-};
+import {
+  initialState,
+  demoUser,
+} from "../../../tests/fixtures/userSliceFixtures";
 
 describe("userSlice", () => {
   it("should set login state", () => {
@@ -22,18 +20,10 @@ describe("userSlice", () => {
   });
 
   it("should set current user", () => {
-    const user = {
-      id: 1,
-      name: "Thulio",
-      email: "thulio@gmail.com",
-      image: "user.png",
-      questionsCount: 5,
-      answersCount: 4,
-    };
-    const state = userSlice.reducer(initialState, setCurrentUser(user));
+    const state = userSlice.reducer(initialState, setCurrentUser(demoUser));
     expect(state).toEqual({
       ...initialState,
-      user,
+      user: demoUser,
     });
   });
   it("should set token", () => {
