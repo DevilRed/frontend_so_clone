8a684b49d4eaad32f76e5375db701178f9ae1272
Christian <chrisdaq@gmail.com>
2025-02-03 17:01:49 -0400

    use test util function on App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,47 +1,11 @@
 import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import userReducer from "./redux/slices/userSlice";
-import questionsReducer from "./redux/slices/questionSlice";
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../tests/test-util";
 import App from "./App";
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    questions: questionsReducer,
-  },
-  preloadedState: {
-    user: {
-      isLoggedIn: false,
-      token: "",
-      user: null,
-    },
-    questions: {
-      questions: {
-        data: [], // List of questions
-        meta: null, // Pagination metadata
-      },
-      question: null,
-      loading: false,
-      page: 1,
-      error: null,
-      choosenTag: "",
-      choosenUser: "",
-      nexPageLink: null,
-      prevPageLink: null,
-      showAll: false,
-    },
-  },
-});
 
 describe("App", () => {
   it("should render ok", () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-    expect(getByText("React Stack")).toBeInTheDocument();
+    renderWithProviders(<App />);
+    expect(screen.getByText("React Stack")).toBeInTheDocument();
   });
 });
